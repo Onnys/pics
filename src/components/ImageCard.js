cdb10d2e27f09a7a6babfadce486a5154b05e93b
onnys <onnysanild@gmail.com>
2022-03-22 13:32:34 +0200

    add gridlayout

---
+++ src/components/ImageCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ImagesList.css"
+
+class ImageCard extends React.Component {
+  constructor(props){
+      super(props);
+      this.imageRef = React.createRef();
+  }
+  componentDidMount(){
+      this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+  setSpans=()=>{
+      console.log(this.imageRef.current.clientHeight)
+  }
+    render() {
+    return (
+      <div>
+        <img
+        ref={this.imageRef}
+        
+          alt={this.props.image.description}
+          src={this.props.image.urls.regular}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
